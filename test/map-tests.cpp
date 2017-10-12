e811b69f6c1aaae353553ac6c36b4f215481894d
arosspope <andrew.pope456@gmail.com>
2017-10-13 09:10:57 +1100

    Tests for creating image maps

--- test/map-tests.cpp
+++ test/map-tests.cpp
@@ -1,5 +1,84 @@
 #include "gtest/gtest.h"
+#include "../src/localmap.h"
 #include "../src/graph.h"
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+cv::Mat blankMap(void){
+  //This map is completely white
+  double mapSize = 20.0;
+  double res = 0.1;
+  int pixels = (int) mapSize / res;
+
+  cv::Mat image(pixels, pixels, CV_8UC1, cv::Scalar(255, 255, 255));
+
+  return image;
+}
+
+cv::Mat partionedMap(void){
+  //This map has a horizontal black line through the middle
+  double mapSize = 20.0;
+  double res = 0.1;
+  int pixels = (int) mapSize / res;
+
+  cv::Mat image(pixels, pixels, CV_8UC1, cv::Scalar(255, 255, 255));
+  cv::line(image,cv::Point(0,100),cv::Point(200,100),cv::Scalar(0,0,0),1);
+
+  return image;
+}
+
+cv::Mat unknownMap(void){
+  //Contains two 'unknown' (grey) areas on the map
+  double mapSize = 20.0;
+  double res = 0.1;
+  int pixels = (int) mapSize / res;
+
+  cv::Mat image(pixels, pixels, CV_8UC1, cv::Scalar(255, 255, 255));
+  cv::rectangle(image, cv::Point(10,10),cv::Point(70,75),cv::Scalar(125,125,125),-1);
+  cv::rectangle(image, cv::Point(80,80),cv::Point(200,200),cv::Scalar(125,125,125),-1);
+
+  return image;
+}
+
+TEST(LocalMap, pointsConnect){
+//  LocalMap l;
+
+//  cv::Mat img = cv::imread("../empty.jpg"); //200x283px
+
+//  TLocalOrd pt1 = {50, 60};
+//  TLocalOrd pt2 = {100, 80};
+
+//  cv::imshow("Before", img); //TODO: Test code
+//  //ASSERT_TRUE(l.canConnect(img, pt1, pt2));
+//  cv::imshow("After", img); //TODO: Test code
+
+}
+
+TEST(ImageGen, CorrectDimensions){
+  double mapSize=20.0;
+  double resolution=0.1;
+
+  int pixels = (int) mapSize / resolution;
+
+  // Create a simple blank map
+  cv::Mat image = blankMap();
+
+  // Let's check map size compared to allocation, just in case
+  ASSERT_EQ(pixels, image.rows);
+  ASSERT_EQ(pixels, image.cols);
+
+  // Let's check the map is blank
+  ASSERT_EQ(255,image.at<uchar>(0,0));
+
+  // Draw a link from top left to botom right corner
+  cv::line(image,cv::Point(0,0),cv::Point(pixels,pixels),cv::Scalar(0,0,0),1);
+
+  // Let's check the centre is now black (0)
+  ASSERT_EQ(0,image.at<uchar>(pixels/2,pixels/2));
+}
 
 //These tests are based on the graph examples found
 //on the website: https://brilliant.org/wiki/dijkstras-short-path-finder/
@@ -19,24 +98,18 @@ TEST(Graphs, FoundPath1){
   g.addEdge(0, 1, 3.0);
   g.addEdge(0, 2, 7.0);
   g.addEdge(0, 3, 5.0);
-
   g.addEdge(1, 4, 7.0);
   g.addEdge(1, 2, 1.0);
-
   g.addEdge(2, 3, 3.0);
   g.addEdge(2, 4, 2.0);
   g.addEdge(2, 5, 1.0);
   g.addEdge(2, 6, 3.0);
-
   g.addEdge(3, 6, 2.0);
-
   g.addEdge(4, 5, 2.0);
   g.addEdge(4, 7, 1.0);
-
   g.addEdge(5, 7, 3.0);
   g.addEdge(5, 6, 3.0);
   g.addEdge(5, 8, 2.0);
-
   g.addEdge(6, 8, 4.0);
   g.addEdge(7, 8, 5.0);
 
@@ -65,21 +138,16 @@ TEST(Graphs, FoundPath2){
   g.addEdge(0, 1, 3.0);
   g.addEdge(0, 2, 7.0);
   g.addEdge(0, 3, 5.0);
-
   g.addEdge(1, 4, 7.0);
   g.addEdge(1, 2, 1.0);
-
   g.addEdge(2, 3, 3.0);
   g.addEdge(2, 4, 2.0);
   g.addEdge(2, 5, 1.0);
   g.addEdge(2, 6, 3.0);
-
   g.addEdge(4, 5, 2.0);
   g.addEdge(4, 7, 1.0);
-
   g.addEdge(5, 7, 3.0);
   g.addEdge(5, 6, 3.0);
-
   g.addEdge(6, 8, 4.0);
   g.addEdge(7, 8, 5.0);
 
@@ -108,21 +176,16 @@ TEST(Graphs, UnorderedVerticies){
   g.addEdge(10, 11, 3.0);
   g.addEdge(10, 12, 7.0);
   g.addEdge(10, 13, 5.0);
-
   g.addEdge(11, 25, 7.0);
   g.addEdge(11, 12, 1.0);
-
   g.addEdge(12, 13, 3.0);
   g.addEdge(12, 25, 2.0);
   g.addEdge(12, 15, 1.0);
   g.addEdge(12, 16, 3.0);
-
   g.addEdge(25, 15, 2.0);
   g.addEdge(25, 17, 1.0);
-
   g.addEdge(15, 17, 3.0);
   g.addEdge(15, 16, 3.0);
-
   g.addEdge(16, 50, 4.0);
   g.addEdge(17, 50, 5.0);
 
