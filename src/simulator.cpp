130be74fc9be3e678f5c6d442261beef463bff26
arosspope <andrew.pope456@gmail.com>
2017-10-17 10:37:19 +1100

    wip on simulator

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,9 +1,6 @@
 /*! @file
  *
- *  @brief An interface for accessing information about the robot's world.
- *
- *  Using roscore to connect to other ros nodes, this object
- *  obtains information from /odom (robot's pose), and /map_image/full (OgMap).
+ *  @brief TODO
  *
  *  @author arosspope
  *  @date 12-10-2017
@@ -12,6 +9,7 @@
 #include "sensor_msgs/image_encodings.h"
 #include "geometry_msgs/PoseArray.h"
 #include "nav_msgs/Odometry.h"
+#include "prm_sim/RequestGoal.h"
 
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -22,12 +20,30 @@
 namespace enc = sensor_msgs::image_encodings;
 
 Simulator::Simulator(ros::NodeHandle nh, TWorldInfoBuffer &buffer):
-  buffer_(buffer), nh_(nh)
+  buffer_(buffer), nh_(nh), it_(nh)
 {
-  path_ = nh_.advertise<geometry_msgs::PoseArray>("/path", 1);
+  path_     = nh_.advertise<geometry_msgs::PoseArray>("path", 1);
+  overlay_  = it_.advertise("prm", 1);
 
-//  odom_ = nh_.subscribe("odom", 1000, &Simulator::odomCallBack, this);
+  reqGoal_ = nh_.advertiseService("request_goal", &Simulator::requestGoal, this);
 
-//  image_transport::ImageTransport it(nh);
-//  ogmap_ = it.subscribe("map_image/full", 1, &Simulator::ogMapCallBack, this);
+  //Get parameters from command line
+  ros::NodeHandle pn("~");
+  double mapSize;
+  double mapResolution;
+  double robotDiameter;
+  pn.param<double>("map_size", mapSize, 20.0);
+  pn.param<double>("resolution", mapResolution, 0.1);
+  pn.param<double>("robot_diameter", robotDiameter, 0.2);
+
+  gmap_ = GlobalMap(mapSize, mapResolution, robotDiameter);
+}
+
+
+bool Simulator::requestGoal(prm_sim::RequestGoal::Request &req, prm_sim::RequestGoal::Response &res)
+{
+  ROS_INFO("request: x=%ld, y=%ld", (long int)req.x, (long int)req.y);
+  res.ack = true;
+  ROS_INFO("sending back response: [%d]", res.ack);
+  return true;
 }
--- src/simulator.h
+++ src/simulator.h
@@ -11,6 +11,8 @@
 #include <image_transport/image_transport.h>
 #include "ros/ros.h"
 #include "types.h"
+#include "globalmap.h"
+#include "prm_sim/RequestGoal.h"
 
 //TODO: Should this be called simulator??
 class Simulator
@@ -27,8 +29,17 @@ private:
   ros::NodeHandle nh_;                      /*!< The handle of the ros node using this class */
   TWorldInfoBuffer &buffer_;                /*!< A shared global structure to update with world information */
   ros::ServiceServer reqGoal_;              /*!< Advertises a service '/request_goal' to set the goal */
-  image_transport::ImageTransport overlay_; /*!< Publishes an overlay of the prm on top of the OgMap to /prm */
+
+  image_transport::ImageTransport it_;      /*!< Transport mechanism for images */
+  image_transport::Publisher overlay_;      /*!< Publishes an overlay of the prm on top of the OgMap to /prm */
   ros::Publisher path_;                     /*!< Publishes the path between robot and goal on /path */
+
+  GlobalMap gmap_;
+
+  //condition var to wait on new goals
+
+
+  bool requestGoal(prm_sim::RequestGoal::Request &req, prm_sim::RequestGoal::Response &res);
 };
 
 #endif // SIMULATOR_H
